c8b0dd9638af3eb5b056f24851eddb8439e3fed9
RodLA <rodrigoloayzab@gmail.com>
2022-11-04 22:18:16 -0500

    Api user register, login and authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+    public function login(UserLoginRequest $request){
+
+        $credentials = $request->only('email','password');
+
+        if( !Auth::attempt( $credentials ) ){
+            return response([
+                "message" => "Usuario y/o password incorrecto"
+            ],422);
+        }
+
+        $accessToken = Auth::user()->createToken('authTestToken')->accessToken;
+
+        return response([
+            "user" => Auth::user(),
+            "access_token" => $accessToken
+        ]);
+    }
+    public function register(UserRegisterRequest $request){
+
+        DB::beginTransaction();
+        try {
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            DB::commit();
+
+            $success = true;
+            $message = "Registration successfully";
+        } catch (\Exception $e) {
+            //Para retroceder la insercion de datos
+            DB::rollback();
+
+            $success = false;
+            $message = "Registration failed";
+        }
+
+        $response = [
+            'success' => $success,
+            'message' => $message
+        ];
+
+        return response()->json( $response );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user', function(Request $request){
-    return response()->json(['success'=>true]);
-});
+Route::post('/login', 'App\Http\Controllers\AuthController@login')->name('api.login');
+Route::post('/register', 'App\Http\Controllers\AuthController@register')->name('api.register');
