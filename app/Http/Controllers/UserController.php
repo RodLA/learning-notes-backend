0bcbd65f90b30f57f5bdae89f997dbd0534ad019
RodLA <rodrigoloayzab@gmail.com>
2023-02-12 12:37:53 -0500

    api and actions for controller of change of name and password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,31 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Actions\User\UpdateUserDetailsAction;
+use App\Actions\User\UpdateUserPasswordAction;
 
 class UserController extends Controller
 {
     public function me(){
         // return Auth::user();
         return new UserResource(Auth::user());
+    }
+
+    public function changePassword( Request $request,UpdateUserPasswordAction $updateUserPasswordAction){
+
+        if( $updateUserPasswordAction->run( $request->all() , Auth::id() ) ){
+            return response()->json([ "success" => true]);
+        }
+
+        return response()->json([ "success" => false]);
+    }
+    public function changeDetails( Request $request, UpdateUserDetailsAction $updateUserDetailsAction ){
+
+        if( $updateUserDetailsAction->run($request->all(), Auth::id() ) ){
+            return response()->json([ "success" => true ]);
+        }
+
+        return response()->json([ "success" => false ]);
     }
 }
